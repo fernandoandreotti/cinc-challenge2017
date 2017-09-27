36390a0f317dd8843c8c98ad2273397c1157bab7
Fernando Andreotti <fernandoandreotti@users.noreply.github.com>
2017-09-27 14:18:26 +0100

    Update train_model.py

--- deeplearn-approach/train_model.py
+++ deeplearn-approach/train_model.py
@@ -1,3 +1,38 @@
+'''
+This function  function used for training and cross-validating model using. The database is not 
+included in this repo, please download the CinC Challenge database and truncate/pad data into a 
+NxM matrix array, being N the number of recordings and M the window accepted by the network (i.e. 
+30 seconds).
+
+
+For more information visit: https://github.com/fernandoandreotti/cinc-challenge2017
+ 
+ Referencing this work
+   Andreotti, F., Carr, O., Pimentel, M.A.F., Mahdi, A., & De Vos, M. (2017). Comparing Feature Based 
+   Classifiers and Convolutional Neural Networks to Detect Arrhythmia from Short Segments of ECG. In 
+   Computing in Cardiology. Rennes (France).
+--
+ cinc-challenge2017, version 1.0, Sept 2017
+ Last updated : 27-09-2017
+ Released under the GNU General Public License
+ Copyright (C) 2017  Fernando Andreotti, Oliver Carr, Marco A.F. Pimentel, Adam Mahdi, Maarten De Vos
+ University of Oxford, Department of Engineering Science, Institute of Biomedical Engineering
+ fernando.andreotti@eng.ox.ac.uk
+   
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
