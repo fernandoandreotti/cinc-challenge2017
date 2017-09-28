c700f242adf8c7e7bb0df1a56319c2658386ca0c
Fernando Andreotti <fernando.andreotti@eng.ox.ac.uk>
2017-09-28 16:03:22 +0100

    Fixed function to transform multiple data into one file

--- deeplearn-approach/cinc_traindb.py
+++ deeplearn-approach/cinc_traindb.py
@@ -40,28 +40,38 @@ import numpy as np
 import glob
 
 # Parameters
-FS = 300
-WINDOW_SIZE = 30*FS
-
-## Searching files
 dataDir = '/some_path/' # <---- change!!
+dataDir = 'training2017/'
+FS = 300
+WINDOW_SIZE = 60*FS
 
+
+## Loading time serie signals
 files = sorted(glob.glob(dataDir+"*.mat"))
-
 trainset = np.zeros((len(files),WINDOW_SIZE))
+count = 0
 for f in files:
     record = f[:-4]
     record = record[-6:]
     # Loading
     mat_data = scipy.io.loadmat(f[:-4] + ".mat")
     print('Loading record {}'.format(record))    
-    data = mat_data['val']
+    data = mat_data['val'].squeeze()
     # Preprocessing
     print('Preprocessing record {}'.format(record))       
     data = np.nan_to_num(data) # removing NaNs and Infs
-    data = data[0,0:WINDOW_SIZE]
     data = data - np.mean(data)
     data = data/np.std(data)
-    trainset[f,:len(data)] = data.T # padding sequence
+    trainset[count,:min(WINDOW_SIZE,len(data))] = data[:min(WINDOW_SIZE,len(data))].T # padding sequence
+    count += 1
     
-#scipy.io.savemat('CNNresults.mat',mdict={'labels': answermat,'probs': probmat})
+## Loading labels    
+import csv
+csvfile = list(csv.reader(open(dataDir+'REFERENCE.csv')))
+traintarget = np.zeros((trainset.shape[0],4))
+classes = ['A','N','O','~']
+for row in range(len(csvfile)):
+    traintarget[row,classes.index(csvfile[row][1])] = 1
+            
+# Saving both
+scipy.io.savemat('trainigset.mat',mdict={'trainset': trainset,'traintarget': traintarget})
