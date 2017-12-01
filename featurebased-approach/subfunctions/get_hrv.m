9821b1dbefcbcf3dde17ec7b7a32888126901102
Fernando Andreotti <fernando.andreotti@eng.ox.ac.uk>
2017-12-01 14:47:09 +0000

    Further minor corrections to files

--- featurebased-approach/subfunctions/get_hrv.m
+++ featurebased-approach/subfunctions/get_hrv.m
@@ -1,7 +1,7 @@
 function HRV=get_hrv(hrv_now)
 % Loads QRS peak detector location file and calculates all heart rate
 % variability features (HRV).
-
+%
 % Time domain features:
 %               SDNN: Standard deviation of all NN intervals
 %              SDANN: Standard deviation of mean of NN intervals in 5 min
@@ -34,7 +34,7 @@ function HRV=get_hrv(hrv_now)
 %                SD2: Standard deviation in y=x direction of Poincare plot
 %               saen: Sample Entropy
 %               apen: Approximate Entropy
-
+%
 %            Recurrence Quantification Analysis 
 %                 RR: Recurrence Rate
 %                Det: Determinism
@@ -44,7 +44,41 @@ function HRV=get_hrv(hrv_now)
 %               TKEO: Mean Teager-Kaiser Energy Operator%   
 %             DFA_a2: Detrended Fluctuation Analysis Exponent%                 
 %                 LZ: Lempel Ziv Complexity
-
+%
+%
+% --
+% ECG classification from single-lead segments using Deep Convolutional Neural 
+% Networks and Feature-Based Approaches - December 2017
+% 
+% Released under the GNU General Public License
+%
+% Copyright (C) 2017  Fernando Andreotti, Oliver Carr
+% University of Oxford, Insitute of Biomedical Engineering, CIBIM Lab - Oxford 2017
+% fernando.andreotti@eng.ox.ac.uk
+%
+% 
+% For more information visit: https://github.com/fernandoandreotti/cinc-challenge2017
+% 
+% Referencing this work
+%
+% Andreotti, F., Carr, O., Pimentel, M.A.F., Mahdi, A., & De Vos, M. (2017). 
+% Comparing Feature Based Classifiers and Convolutional Neural Networks to Detect 
+% Arrhythmia from Short Segments of ECG. In Computing in Cardiology. Rennes (France).
+%
+% Last updated : December 2017
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 % load QRS time data
